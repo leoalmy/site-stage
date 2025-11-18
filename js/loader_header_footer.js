a94dfa4b2c7fa9c6e8bb01307b993433e7e1e27d
Leo ALMY <leo.almy@edouardgand.fr>
2025-11-18 15:38:35 +0100

    Amélioration système inclusion

--- js/loader_header_footer.js
+++ js/loader_header_footer.js
@@ -1,21 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const headerContainer = document.getElementById('header-container');
-    if (!headerContainer) return; // si pas de conteneur, on ne fait rien
-
-    fetch('header.html')
-        .then(response => response.text())
-        .then(data => {
-            headerContainer.innerHTML = data;
-        })
-        .catch(err => console.error('Erreur lors du chargement de l\'entete :', err))
-
-    const footerContainer = document.getElementById('footer-container');
-    if (!footerContainer) return; // si pas de conteneur, on ne fait rien
-
-    fetch('footer.html')
-        .then(response => response.text())
-        .then(data => {
-            footerContainer.innerHTML = data;
-        })
-        .catch(err => console.error('Erreur lors du chargement du pied de page :', err));
+    const includes = document.querySelectorAll('[data-include]');
+    includes.forEach(el => {
+        const file = el.getAttribute('data-include');
+        fetch(file)
+            .then(resp => resp.text())
+            .then(data => {
+                el.innerHTML = data;
+            })
+            .catch(err => console.error(`Erreur lors du chargement de ${file} :`, err));
+    });
 });
